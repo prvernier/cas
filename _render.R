bfcaabd4e4b6c497c26d33461866b95ddbc4d776
Pierre R Vernier <pierre.vernier@gmail.com>
2019-07-30 12:56:19 -0700

    Fixed error in nb01_eco.Rmd

--- _render.R
+++ _render.R
@@ -6,10 +6,10 @@ source("R/casRender.R")
 #render("index.Rmd", output_dir="docs")
 #render("cas_specs.Rmd", output_dir="docs")
 
-x=friConnect("bc08")
+#x=friConnect("bc08")
 #casRender("bc08")
 
-#x=friConnect("nb01")
+x=friConnect("nb01")
 #casRender("nb01")
 
 #x=friConnect("ab06")
