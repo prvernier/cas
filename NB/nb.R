4aee4aa42d3834a26174a79efef0f2eb3a2d3e6a
Pierre R Vernier <pierre.vernier@gmail.com>
2019-12-13 12:39:47 -0500

    Update nb.R

--- NB/nb.R
+++ NB/nb.R
@@ -3,8 +3,8 @@ library(tidyverse)
 library(summarytools)
 
 con = dbConnect(RPostgreSQL::PostgreSQL(), dbname="casfri50_pierrev", host="localhost", port=5432, user="postgres", password="1postgres")
-x1 = as_tibble(dbGetQuery(con, "SELECT * FROM rawfri.nb01")) # LIMIT 10000"))
-#x2 = as_tibble(dbGetQuery(con, "SELECT * FROM rawfri.nb02")) # LIMIT 10000"))
+x1 = as_tibble(dbGetQuery(con, "SELECT * FROM rawfri.nb01"))
+x2 = as_tibble(dbGetQuery(con, "SELECT * FROM rawfri.nb02"))
 dbDisconnect(con)
 
 sink("NB/nb01.txt")
