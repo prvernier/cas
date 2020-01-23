ab3b44507b25866522d9d750bb73b4223b7fa4b0
Pierre R Vernier <pierre.vernier@gmail.com>
2020-01-23 18:26:57 -0500

    Updates to bc10

---
+++ BC10/bc10.R
@@ -0,0 +1,147 @@
+library(sf)
+library(DT)
+library(plyr)
+library(shiny)
+library(leaflet)
+library(mapview)
+library(rpostgis)
+library(tidyverse)
+library(shinydashboard)
+
+mapviewOptions(basemaps=c("Esri.WorldImagery","Esri.NatGeoWorldMap"), layers.control.pos="topright")
+casVars = c("stand_structure", "num_of_layers", "soil_moist_reg", "crown_closure_upper", "crown_closure_lower", "height_upper", "species_1", "species_per_1", "origin_upper", "height_lower", "origin_upper", "origin_lower", "site_inde", "non_for_veg", "nat_non_veg", "non_for_anth")
+sppList = read_csv("../CASFRI/translation/tables/lookup/bc_vri01_species.csv")
+
+if (!exists("bc10")) {
+    con = dbConnect(RPostgreSQL::PostgreSQL(), dbname="casfri50_pierrev", host="localhost", port=5432, user="postgres", password="1postgres")
+    bc10 = st_read(con, query="SELECT * FROM rawfri.bc10 ORDER BY random() LIMIT 10000;") %>%
+            select(inventory_standard_cd, projected_date, for_mgmt_land_base_ind, soil_moisture_regime_1, bec_zone_code, bec_subzone, bec_variant, bec_phase,
+            map_id, l1_feature_id, line_7b_disturbance_history, feature_area_sqm, feature_length_m, reference_year, site_position_meso,
+            land_cover_class_cd_1, non_veg_cover_type_1, bclcs_level_4, non_productive_descriptor_cd, l1_non_forest_descriptor,
+            l1_layer_id, l1_for_cover_rank_cd, l1_crown_closure, l1_species_cd_1, l1_species_cd_2, l1_species_pct_1, l1_proj_height_1, l1_proj_age_1, l1_site_index, l1_est_site_index,
+            l2_layer_id, l2_for_cover_rank_cd, l2_crown_closure, l2_species_cd_1, l2_species_pct_1, l2_proj_height_1, l2_proj_age_1, l2_site_index, l2_est_site_index,
+            d_layer_id, d_for_cover_rank_cd, d_crown_closure, d_species_cd_1, d_species_pct_1, d_proj_height_1, d_proj_age_1, d_site_index, d_est_site_index)
+    dbDisconnect(con)
+}
+
+bc10 = mutate(bc10,
+        l1_proj_height_1 = round(l1_proj_height_1, 1),
+        l1_site_index = round(l1_site_index, 1),
+
+        orig_stand_id = l1_feature_id,
+
+        stand_structure = case_when(
+            l2_layer_id==2 ~ "M",
+            l1_layer_id==1 ~ "S",
+            TRUE ~ "NULL_VALUE"),
+
+        num_of_layers = case_when(
+            l2_layer_id==2 ~ 2,
+            l1_layer_id==1 ~ 1,
+            TRUE ~ -8888),
+
+        identification_id = mapvalues(inventory_standard_cd, c("F","V","I","L"), c(4,5,6,7)),
+
+        map_sheet_id = map_id,
+
+        gis_area= round(feature_area_sqm/10000,1),
+
+        gis_perimeter = round(feature_length_m,0),
+
+        inventory_area = round(feature_area_sqm/10000,1),
+
+        photo_year = reference_year,
+
+        soil_moist_reg = case_when(
+            is.na(soil_moisture_regime_1) ~ "NULL_VALUE",
+            str_trim(soil_moisture_regime_1)=="" ~ "EMPTY_STRING",
+            soil_moisture_regime_1 %in% c(0,1,2) ~ "D",
+            soil_moisture_regime_1 %in% c(3,4) ~ "F",
+            soil_moisture_regime_1 %in% c(5,6) ~ "M",
+            soil_moisture_regime_1 %in% c(7,8) ~ "W",
+            TRUE ~ "NOT_IN_SET"),
+        
+        crown_closure_upper=case_when(
+            is.na(l1_crown_closure) ~ as.integer(-8888),
+            l1_crown_closure < 0 | l1_crown_closure > 100 ~ as.integer(-9999),
+            TRUE ~ l1_crown_closure),
+        crown_closure_lower=crown_closure_upper,
+        
+        height_upper=case_when(
+            is.na(l1_proj_height_1) ~ -8888,
+            l1_proj_height_1 < 0.1 | l1_proj_height_1 > 100 ~ -9999,
+            TRUE ~ l1_proj_height_1),
+        height_lower=height_upper,
+
+        species_1=case_when(
+            is.na(l1_species_cd_1) ~ "NULL_VALUE",
+            str_trim(l1_species_cd_1)=="" ~ "EMPTY_STRING",
+            !l1_species_cd_1 %in% sppList$source_val ~ "NOT_IN_SET",
+            TRUE ~ plyr::mapvalues(l1_species_cd_1, sppList$source_val, sppList$spec1)),
+
+        species_per_1=case_when(
+            is.na(l1_species_pct_1) ~ -8888,
+            l1_species_pct_1 < 0 | l1_species_pct_1 > 100 ~ -9999,
+            TRUE ~ l1_species_pct_1),
+
+        projected_year=as.double(substr(projected_date, 1, 4)),
+        origin_upper=case_when(
+            is.na(l1_proj_age_1) ~ -8888,
+            l1_proj_age_1==0 ~ -9999,
+            TRUE ~ projected_year - l1_proj_age_1),
+        origin_lower=origin_upper,
+
+        site_index=case_when(
+            is.na(l1_site_index) & is.na(l1_est_site_index) ~ -8888,
+            is.na(l1_site_index) & !is.na(l1_est_site_index) ~ l1_est_site_index,
+            TRUE ~ l1_site_index),
+
+        non_for_veg="NULL_VALUE", 
+        nat_non_veg="NULL_VALUE", 
+        non_for_anth="NULL_VALUE", 
+        un_prod_for="NULL_VALUE",
+
+    # Process where inventory_standard_cd = "V" or "I"
+    # ------------------------------------------------
+
+    # Non-forested vegetated (land_cover_class_cd_1 THEN bclcs_level_4)
+    non_for_veg = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(land_cover_class_cd_1), 
+                if_else(land_cover_class_cd_1 %in% c("BL","BM","BY","HE","HF","HG","SL","ST"), mapvalues(land_cover_class_cd_1,c("BL","BM","BY","HE","HF","HG","SL","ST"),c("BR","BR","BR","HE","HF","HG","SL","ST")), "NULL_VALUE"), "NULL_VALUE"),
+    non_for_veg = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(bclcs_level_4) & non_for_veg=="NULL_VALUE",
+                if_else(bclcs_level_4 %in% c("BL","BM","BY","HE","HF","HG","SL","ST"), mapvalues(bclcs_level_4,c("BL","BM","BY","HE","HF","HG","SL","ST"),c("BR","BR","BR","HE","HF","HG","SL","ST")), non_for_veg), non_for_veg),
+
+    # Non-vegetated natural (non_veg_cover_type_1 THEN land_cover_class_cd_1 THEN bclcs_level_4)
+    nat_non_veg = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(non_veg_cover_type_1), 
+                if_else(non_veg_cover_type_1 %in% c("BE","BI","BR","BU","CB","DW","ES","GL","LA","LB","LL","LS","MN","MU","OC","PN","RE","RI","RM","RS","TA"), mapvalues(non_veg_cover_type_1,c("BE","BI","BR","BU","CB","DW","ES","GL","LA","LB","LL","LS","MN","MU","OC","PN","RE","RI","RM","RS","TA"),c("BE","RK","RK","EX","EX","DW","EX","SI","LA","RK","EX","WS","EX","WS","OC","SI","LA","RI","EX","WS","RK")), "NULL_VALUE"), "NULL_VALUE"),
+    nat_non_veg = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(land_cover_class_cd_1) & nat_non_veg=="NULL_VALUE", 
+                if_else(land_cover_class_cd_1 %in% c("BE","BI","BR","BU","CB","EL","ES","GL","LA","LB","LL","LS","MN","MU","OC","PN","RE","RI","RM","RO","RS","SI","TA"), mapvalues(land_cover_class_cd_1,c("BE","BI","BR","BU","CB","EL","ES","GL","LA","LB","LL","LS","MN","MU","OC","PN","RE","RI","RM","RO","RS","SI","TA"),c("BE","RK","RK","EX","EX","EX","EX","SI","LA","RK","EX","WS","EX","WS","OC","SI","LA","RI","EX","RK","WS","SI","RK")), nat_non_veg), nat_non_veg),
+    nat_non_veg = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(bclcs_level_4) & nat_non_veg=="NULL_VALUE",
+                if_else(bclcs_level_4 %in% c("EL","RO","SI"), mapvalues(bclcs_level_4,c("EL","RO","SI"),c("EX","RK","SI")), nat_non_veg), nat_non_veg),
+
+    # Non-vegetated anthropogenic(non_veg_cover_type_1 THEN land_cover_class_cd_1)
+    non_for_anth = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(non_veg_cover_type_1),
+                 if_else(non_veg_cover_type_1 %in% c("AP","GP","MI","MZ","OT","RN","RZ","TZ","UR"), mapvalues(non_veg_cover_type_1,c("AP","GP","MI","MZ","OT","RN","RZ","TZ","UR"),c("FA","IN","IN","IN","OT","FA","FA","IN","FA")), "NULL_VALUE"), "NULL_VALUE"),
+    non_for_anth = if_else((inventory_standard_cd=="V" | inventory_standard_cd=="I") & !is.na(land_cover_class_cd_1) & non_for_anth=="NULL_VALUE", 
+                 if_else(land_cover_class_cd_1 %in% c("AP","GP","MI","MZ","OT","RN","RZ","TZ","UR"), mapvalues(land_cover_class_cd_1,c("AP","GP","MI","MZ","OT","RN","RZ","TZ","UR"),c("FA","IN","IN","IN","OT","FA","FA","IN","FA")), non_for_anth), non_for_anth),
+
+    # Process where inventory_standard_cd=="F"
+    # ----------------------------------------
+
+    # Non-forested vegetated (non_productive_descriptor_cd THEN bclcs_level_4)
+    non_for_veg = if_else(inventory_standard_cd=="F" & !is.na(non_productive_descriptor_cd), 
+                if_else(non_productive_descriptor_cd %in% c("AF","M","NPBR","OR"), mapvalues(non_productive_descriptor_cd,c("AF","M","NPBR","OR"),c("AF","HG","ST","HG")), "NULL_VALUE"), non_for_veg),
+    non_for_veg = if_else(inventory_standard_cd=="F" & !is.na(bclcs_level_4) & non_for_veg=="NULL_VALUE",
+                if_else(bclcs_level_4 %in% c("BL","BM","BY","HE","HF","HG","SL","ST"), mapvalues(bclcs_level_4,c("BL","BM","BY","HE","HF","HG","SL","ST"),c("BR","BR","BR","HE","HF","HG","SL","ST")), non_for_veg), non_for_veg),
+
+    # Non-vegetated natural (non_productive_descriptor_cd THEN bclcs_level_4)
+    nat_non_veg = if_else(inventory_standard_cd=="F" & !is.na(non_productive_descriptor_cd), 
+                if_else(non_productive_descriptor_cd %in% c("A","CL","G","ICE","L","MUD","R","RIV","S","SAND","TIDE"), mapvalues(non_productive_descriptor_cd,c("A","CL","G","ICE","L","MUD","R","RIV","S","SAND","TIDE"),c("AP","EX","WS","SI","LA","EX","RK","RI","SL","SA","TF")), "NULL_VALUE"), nat_non_veg),
+    nat_non_veg = if_else(inventory_standard_cd=="F" & !is.na(bclcs_level_4) & nat_non_veg=="NULL_VALUE", 
+                if_else(bclcs_level_4 %in% c("EL","RO","SI"), mapvalues(bclcs_level_4,c("EL","RO","SI"),c("EX","RK","SI")), nat_non_veg), nat_non_veg),
+
+    # Non-vegetated anthropogenic(non_productive_descriptor_cd)
+    non_for_anth = if_else(inventory_standard_cd=="F" & !is.na(non_productive_descriptor_cd),
+                 if_else(non_productive_descriptor_cd %in% c("C","GR","P","U"), mapvalues(non_productive_descriptor_cd,c("C","GR","P","U"),c("CL","IN","CL","FA")), "NULL_VALUE"), non_for_anth)
+)
+
+shiny::runApp("shiny")
--- shiny/app.R
+++ shiny/app.R
@@ -1,20 +1,33 @@
-source("global.R")
-
 ui = dashboardPage(
   dashboardHeader(title = "CASFRI Explorer"),
   dashboardSidebar(
-    sidebarMenu(menuItem("Connect to PostGIS", tabName = "fri", icon = icon("th"))),
+    sidebarMenu(
+        menuItem("Connect to PostGIS", tabName = "fri", icon = icon("th"))
+    ),
     selectInput("inv", label = "Inventory:", choices = c("ab06","ab16","bc08","bc10","nb01","nb02","nt01","nt02","qc01","qc02","qc03"), selected="bc10"),
-    textInput("id", label="Enter ogc_fid (optional):", value="", width=NULL),
+    textInput("id", label="Select attribute value(s):", value="", width=NULL),
     hr(),
     actionButton("goButton", "Select (random) polygon")
   ),
   dashboardBody(
-        fluidRow(
-            column(8, box(title = "FRI Map", width = NULL, status="primary", leafletOutput("map1", height=800))),
-            column(4, box(title = "FRI Attributes", width = NULL, height=NULL, status="primary", DT::dataTableOutput("tab1")))
+    tabItems(
+        tabItem(tabName="fri",
+            fluidRow(
+                #box(title = "FRI Map", leafletOutput("map1", height=800), width=8),
+                tabBox(
+                    id = "one", width="8",
+                    tabPanel("FRI Map", leafletOutput("map1", height=800))#,
+                    #tabPanel("Summary", pre(includeText("bc10.txt")))
+                ),
+                tabBox(
+                    id = "two", width="4",
+                    tabPanel("FRI Attributes", DT::dataTableOutput("tab1", height=800)),
+                    tabPanel("CAS Attributes", DT::dataTableOutput("tab2", height=800))
+                )
+            )
         )
     )
+  )
 )
 
 server = function(input, output) {
@@ -23,7 +36,7 @@ server = function(input, output) {
         if (input$id=="") {
             n = sample_n(eval(parse(text=input$inv)), 1)
         } else {
-            n = filter(eval(parse(text=input$inv)), ogc_fid==input$id)
+            n = filter(eval(parse(text=input$inv)), eval(parse(text=input$id))) %>% sample_n(1)
         }
     })
 
@@ -38,7 +51,13 @@ server = function(input, output) {
     })
 
 	output$tab1 <- DT::renderDataTable({
-		datatable(dta1(), rownames=F, options=list(dom = 'tip', scrollX = TRUE, scrollY = TRUE, pageLength = 25), class="compact")
+        x = dta1() %>% filter(!Attribute %in% casVars)
+		datatable(x, rownames=F, options=list(dom = 'tip', scrollX = TRUE, scrollY = TRUE, pageLength = 25), class="compact")
+    })
+
+	output$tab2 <- DT::renderDataTable({
+        x = dta1() %>% filter(Attribute %in% casVars)
+		datatable(x, rownames=F, options=list(dom = 'tip', scrollX = TRUE, scrollY = TRUE, pageLength = 25), class="compact")
     })
 
 }
