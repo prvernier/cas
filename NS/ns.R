2f9cd98fe0514fb06d96ffbf3142b413812cbd53
Pierre R Vernier <pierre.vernier@gmail.com>
2020-06-12 19:17:06 -0400

    NS files updates

--- NS/ns.R
+++ NS/ns.R
@@ -5,7 +5,7 @@ library(rpostgis)
 library(summarytools)
 
 
-# NS03 Inventory
+# Read inventory
 con = dbConnect(RPostgreSQL::PostgreSQL(), dbname="casfri50_pierrev", host="localhost", port=5432, user="postgres", password="1postgres")
 ns = RPostgreSQL::dbGetQuery(con, "SELECT * FROM rawfri.ns03")
 sink("NS/ns03.txt")
@@ -18,25 +18,30 @@ for (i in names(sk)) {
 sink()
 dbDisconnect(con)
 
-################################################################################
 
 # General Notes
+
   * two layers (layer 1 + disturbance; NFL)
-  * use new codes for "template" line in source attribute table (https://github.com/edwardsmarc/CASFRI/issues/263)
 
 
-# CAS Attributes
+# Site class
 
-  * STAND_PHOTO_YEAR
-  * STAND_STRUCTURE
-  * NUM_OF_LAYERS
+The CAS attribute SITE_CLASS requires two source attributes (site_sw and site_hw), but since they use the same codes we can't concatenate them:
 
+SITE_CLASS: mapText({site_sw}, {0,1,2,3,4,5,6,7,8,9,10,11,12,13}, {'P','P','P','P','P','M','M','M','M','M','G','G','G','G'})
+SITE_CLASS: mapText({site_hw}, {0,1,2,3,4,5}, {'P','P','M','M','G','G'})
 
 
 # Secondary species
 
-fri = c('S','SH','HS','H")
-cas = c('NOSC SOFT','NOSC SOFT','NOSC HARD','NOSC HARD')
+There can be two layer2 species and species_per created from one source attribute (ss_species):
+
+SPECIES_1: mapText({sp1}, {'S','SH','HS','H'}, {'Soft unkn','Soft unkn','Hard unkn','Hard unkn'})
+SPECIES_PER_1: mapText({ss_species}, {'S','SH','HS','H'}, {'85','60','40','15'})
+SPECIES_2: mapText({ss_species}, {'S','SH','HS','H'}, {'Hard unkn','Hard unkn','Soft unkn','Soft unkn'})
+SPECIES_PER_2: mapText({ss_species}, {'S','SH','HS','H'}, {'15','40','60','85'})
+
+Original Perl code:
 
 if($Sp eq "S") {
     $Sp1="NOSC SOFT";  $spper1=85;  $Sp2="NOSC HARD"; $spper2=15; $spfreq->{$Sp}++;
