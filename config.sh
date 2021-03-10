1c008025285c6b0833ffb319bbf7e022582338d6
Pierre R Vernier <pierre.vernier@gmail.com>
2021-03-10 10:56:36 -0500

    Added validation script and outputs.

---
+++ config.sh
@@ -0,0 +1,19 @@
+pgport=5432
+pgport_pr=5435
+pghost=localhost
+pgdbname=casfri50_pierrev
+pgdbname_pr=casfri50_pierrer_release
+pguser=postgres
+pgpassword=1postgres
+
+targetFRISchema=rawfri
+targetTranslationFileSchema=translation
+
+friDir=D:/FRIs
+#gdalFolder="C:/Program Files/GDAL"
+#gdalFolder="C:/Program Files/GDAL_1.11.4"
+gdalFolder="C:/Program Files/GDAL_3.1.4"
+
+overwriteFRI=True
+overwriteTTables=True
+
